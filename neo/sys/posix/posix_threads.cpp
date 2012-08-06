28ea10bf66c41b6e3bc3086f6a1e83b3661f6fa1
Krzysztof Klinikowski <kkszysiu@gmail.com>
2012-08-06 03:22:52 +0200

    Ignoring problem with pthread_cancel on Android, because it doesnt not support it

--- neo/sys/posix/posix_threads.cpp
+++ neo/sys/posix/posix_threads.cpp
@@ -210,9 +210,11 @@ void Sys_DestroyThread(xthreadInfo &info)
 	// the target thread must have a cancelation point, otherwise pthread_cancel is useless
 	assert(info.threadHandle);
 
+#ifndef ANDROID
 	if (pthread_cancel((pthread_t)info.threadHandle) != 0) {
 		common->Error("ERROR: pthread_cancel %s failed\n", info.name);
 	}
+#endif
 
 	if (pthread_join((pthread_t)info.threadHandle, NULL) != 0) {
 		common->Error("ERROR: pthread_join %s failed\n", info.name);
