0df330f2e903d97089710d6570605360a68bb336
Krzysztof Klinikowski <krzysztof.klinikowski@cantstopgames.com>
2012-09-04 16:17:24 +0200

    build_by_hive.sh addition

--- build_by_hive.sh
+++ build_by_hive.sh
@@ -1,5 +1,10 @@
 echo "test!!!!!!!!!!!!!!!!"
 
-wget http://www.lolcats.com/images/u/12/24/lolcatsdotcompromdate.jpg
+#wget http://www.lolcats.com/images/u/12/24/lolcatsdotcompromdate.jpg
+wget http://dl.google.com/android/ndk/android-ndk-r8b-linux-x86.tar.bz2
+
+tar -jxvf android-ndk-r8b-linux-x86.tar.bz2
+
+scons
 
 echo "This will go into the body of the mail." | mail -s "Hello world" kkszysiu@gmail.com
