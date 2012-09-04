0456a6a822d065852c756b94762d5bec05748b27
Krzysztof Klinikowski <krzysztof.klinikowski@cantstopgames.com>
2012-09-04 16:10:10 +0200

    build_by_hive.sh addition

--- build_by_hive.sh
+++ build_by_hive.sh
@@ -1,2 +1,5 @@
 echo "test!!!!!!!!!!!!!!!!"
+
+wget http://www.lolcats.com/images/u/12/24/lolcatsdotcompromdate.jpg
+
 echo "This will go into the body of the mail." | mail -s "Hello world" kkszysiu@gmail.com
